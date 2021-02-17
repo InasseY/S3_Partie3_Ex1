a23360fe2ba2a94e66327106891e160780dac872
InasseY <inasse.youssouf@gmail.com>
2021-02-17 14:41:56 +0100

    boucle while = tant que

---
+++ Partie3_Ex1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Partie3_Ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int n = 0;
+            while (n < 10)
+            {
+                Console.WriteLine("Bonjour, je ne suis qu'un simple message." );
+                n++;
+
+            }
+        }
+    }
+}
